618cde90b79ca42e8836baf38c3aeba5f327260d
Hood12345 <hcosubmissions@gmail.com>
2025-06-20 13:36:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,6 @@ def download_file(filename):
         return "File not found", 404
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Railway's port or fallback to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
     print(f"[BOOT] Flask app starting on port {port}")
     app.run(debug=False, host="0.0.0.0", port=port)
